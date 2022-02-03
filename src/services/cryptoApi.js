9c0b61139f26a32eae784edc1d0d975880e985ec
Thailson Betim <thailson.betim.tb@gmail.com>
2022-02-03 16:18:50 -0300

    Feat: Initial commit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,17 @@
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/coins",
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     tiers: "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "2b289477e3mshcaa25c9469bf95ep153343jsnb2e5ad35a489",
+//   },
+// };
