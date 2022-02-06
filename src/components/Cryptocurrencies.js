891dff5c7117855bbe60f2632201be43221f9bc8
Thailson Betim <thailson.betim.tb@gmail.com>
2022-02-06 12:06:04 -0300

    Feat: Page Cryptocurrencies

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import { useEffect, useState } from "react";
 
 const Cryptocurrencies = ({ simplified }) => {
-  const count = simplified ? 10 : 50;
-  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
-  const [Cryptos, setCryptos] = useState(cryptoList?.data?.coins);
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [Cryptos, setCryptos] = useState([]);
+  const [SearchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(SearchTerm.toLocaleLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, SearchTerm]);
 
   if (isFetching) return "Loading...";
 
   return (
     <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => {
+              setSearchTerm(e.target.value);
+            }}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className="crypto-card-container">
         {Cryptos?.map((currency) => (
           <Col
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "ffbf900318msh605e591d8066b4dp18b4abjsnc35f2cd3a89b",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptosNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptosNewsQuery } = cryptoNewsApi;
