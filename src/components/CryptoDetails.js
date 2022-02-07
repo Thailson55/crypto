8c4ab0d39925481be6777f3db9bb97c86c1e5ee5
Thailson Betim <thailson.betim.tb@gmail.com>
2022-02-07 00:39:35 -0300

    Feat: News

--- src/components/CryptoDetails.js
+++ src/components/CryptoDetails.js
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  DollarCircleOutlined,
+  FundOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+  CheckOutlined,
+  NumberOutlined,
+  ThunderboltOutlined,
+} from "@ant-design/icons";
+
+import {
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} from "../services/cryptoApi";
 
 const CryptoDetails = () => {
   return <div></div>;
